849edef1d321875290606f4b66700d1aa79df898
Etienne Brun <etienne.brunj@gmail.com>
2021-03-14 15:49:02 +0000

    Fix almost all errors

---
+++ .#holberton.h
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.4866:1615719982
--- get_cs_func.c
+++ get_cs_func.c
@@ -7,29 +7,29 @@
  * @args:
  **/
 
-int (*get_cs_func(char c))(va_list args, char *s) // for conversion specifiers
+int (*get_cs_func(char c))(va_list, char *) // for conversion specifiers
 {
 	print_t array[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"d", print_signed_int};
-		{"i", print_int};
-		{"b", convert_binary};
-		{"u", print_uns_int};
-		{"o", print_octal};
-		{"x", print_hexa};
-		{"X", print_upper_hexa};
-		{"S", print_string};
-		{"p", print_pointer};
-		{"r", print_rev_string};
-		{"R", pirnt_string_ROT13};
+//		{"c", print_char},
+//		{"s", print_string},
+		{"d", print_int},
+		{"i", print_int}
+//		{"b", convert_binary},
+//		{"u", print_uns_int},
+//		{"o", print_octal},
+//		{"x", print_hexa},
+//		{"X", print_upper_hexa},
+//		{"S", print_string},
+//		{"p", print_pointer},
+//		{"r", print_rev_string},
+//		{"R", pirnt_string_ROT13}
 	};
 
 	int i;
-	int size = 14;
+	int size = 2;
 
 	for (i = 0; i < size; i++)
-		if (*array[i].m == args)
-			return (*array[i].func);
+		if (*array[i].m == c)
+			return (array[i].func);
 	return (NULL);
 }
