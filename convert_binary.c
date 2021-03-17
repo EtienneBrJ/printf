eda21229afe7565b57f6f8c46985dae5826bde90
Etienne Brun <etienne.brunj@gmail.com>
2021-03-17 20:10:44 +0000

    binary ok

--- convert_binary.c
+++ convert_binary.c
@@ -4,32 +4,42 @@
 /**
  * convert_binary - convert a decimal number to binary
  *
- * @args: pointer to the current argument
+  * @args: pointer to the current argument
  * @flagstr: tmp buffer
  *
  * Return: string to print
  **/
 char *convert_binary(va_list args, __attribute__((unused))char *flagstr)
 {
-	char *res = "Holberton";
+	char *res;
 	char s[20];
+	char result[20];
 	int ite, n, max;
+
 	unsigned int i = 0;
+
+	if (args == NULL)
+		return (NULL);
 
 	n = va_arg(args, int);
 
 	while (n > 0)
 	{
-		s[i] = n % 2;
+		s[i] = n % 2 + '0';
 		n = n / 2;
 		i++;
 	}
-	res = _calloc(9 + 1, sizeof(char));
 	max = i - 1;
-	printf("%d : s[0] - %d : s[1] - %d : s[2]\n", s[0], s[1], s[2]);
+
+	res = _calloc(i + 1, sizeof(char));
+
 	for (ite = 0; max > -1; ite++, max--, i--)
 	{
-		res[ite] = s[max];
+		result[ite] = s[max];
 	}
+	result[ite] = '\0';
+
+	_strcat(res, result);
+
 	return (res);
 }
