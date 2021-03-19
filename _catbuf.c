89de9363afcdd5c041741f8795347067746c6d8d
OctaveC <octavec@outlook.com>
2021-03-19 13:18:00 +0000

    fixed and cleaned up

---
+++ _catbuf.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * *_catbuf - This function appends the src string to the dest string.
+ * @dest: the destination string
+ * @src: the source string
+ *
+ * Return: A pointer to the dest string
+ */
+int _catbuf(char *dest, char *src)
+{
+	int location = 0;
+	int ite = 0;
+
+	while (dest[location] != '\0')
+		location++;
+
+	while (src != NULL && src[ite] != '\0')
+	{
+		dest[location] = src[ite];
+		ite++;
+		location++;
+	}
+	dest[location] = '\0';
+	return (location);
+}
